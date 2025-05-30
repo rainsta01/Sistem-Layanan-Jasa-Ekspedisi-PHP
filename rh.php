45ac746f49ffec62a17c820aab3f9527136c8821
Rainsta A.P <131755599+rainsta01@users.noreply.github.com>
2025-05-30 21:40:53 +0700

    Create rh.php

---
+++ rh.php
@@ -0,0 +1,14 @@
+<?php
+include 'koneksi.php';
+
+$id_user = 1; 
+$username = 'admin'; //customize this username (silahkan ganti username ini sesuai keinginan Anda)
+$password = '123456'; //customize this password (silahkan ganti password ini sesuai keinginan Anda)
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$role = 'admin';
+$stmt = $conn->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $username, $hash, $role);
+$stmt->execute();
+
+echo "User berhasil ditambahkan!";
